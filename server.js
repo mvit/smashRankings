ef3651011df9f4400aaac033eac87b81de630383
jrsawin <jrsawin@wpi.edu>
2016-12-09 12:45:18 -0500

    starting pulling from api

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var http = require('http')
   , url  = require('url')
   , path = require('path')
   , qs = require('qs')
+  , XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
   , port = 8080
+  
 
 
 var rankings = [{"tag" : "ZettaVolt", "wins": "9999", "losses": "0", "score": "1337", "last":"Dec. 7, 2016"},{"tag" : "Tessa", "wins": "0", "losses": "9999", "score": "last lmao", "last":"Dec. 7, 2016"}]
@@ -31,6 +33,31 @@ var server = http.createServer (function (req, res) {
       res.end('404 not found')
   }
 })
+
+function makeGet(url) {
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    handleRes(req);
+  }
+  req.open('GET', url);
+  req.send;
+}
+
+function handleRes(req) {
+  if( req.readyState !== XMLHttpRequest.DONE )
+    return;
+
+  if(req.status === 200)
+    getTournaments( JSON.parse(req.responseText) );
+}
+
+function getTournaments(tournaments) {
+  //melee
+  console.log(tournaments);
+}
+
+//get all tournaments
+makeGet('http://api.challonge.com/v1/tournaments.json?api_key=2aXYxGwZxmCd16gzgwBEja8QFtR0xd4bR7yCykg8');
 
 server.listen(process.env.PORT || port);
 console.log('listening on 8080')
