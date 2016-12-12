d13e2fdd678b25e64ca963cd64003e18d6302c37
Maurizio Vitale <mauriziovit@gmail.com>
2016-12-12 15:55:04 -0500

    Server reformat
    Now 10% better to look at

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var https = require('https')
   , http = require('http')
   , trueskill = require('trueskill')
   , later = require('later')
-  , port = 8080
   , sqlite3 = require('sqlite3').verbose()
-  
+
 var challongeHost = 'api.challonge.com'
-var APIKey = '2aXYxGwZxmCd16gzgwBEja8QFtR0xd4bR7yCykg8'
-var IDs = [];
-var players = []
-var tags=[]
+  , APIKey = '2aXYxGwZxmCd16gzgwBEja8QFtR0xd4bR7yCykg8'
+  , port = 8080
+  , file = "ranks.db"
+
+var IDs = []
+  , tags=[]
+
 var cnt=0;
 
 var server = http.createServer (function (req, res) {
@@ -39,8 +41,9 @@ var server = http.createServer (function (req, res) {
   }
 })
 
+//SubRoutines
+
 function parseParticipants(list) {
-  //console.log('parsing participants')
   for (i = 0; i < list.length; i++) {
     if (tags.indexOf(list[i].participant.name) == -1) {
       db.run("INSERT INTO players VALUES (?, ?, ?, ?, ?)",cnt, list[i].participant.name, 100, 0, 9999);
@@ -48,8 +51,6 @@ function parseParticipants(list) {
       tags.push(list[i].participant.name)
     }
   }
-  //trueskill.AdjustPlayers(players)
-  //console.log(players)
 }
 
 function buildParticipants(response) {
@@ -73,10 +74,8 @@ function getParticipants(tournament) {
 }
 
 function parseTournaments(tournaments){
-  //console.log(tournaments);
   for (var item = 0; item < tournaments.length; item++)
   {
-    //console.log(tournaments[item].tournament.game_name);
     if (tournaments[item].tournament.game_name == 'Super Smash Bros. Melee')
     {
       if (tournaments[item].tournament.name.toLowerCase().indexOf("singles") != -1)
@@ -85,7 +84,6 @@ function parseTournaments(tournaments){
       }
     }
   }
-  //console.log("IDs = " + IDs);
   for (var i = 0; i < IDs.length; i++)
   {
     getParticipants(IDs[i]);
@@ -100,12 +98,10 @@ function sendRankings(res) {
 
 function buildTournaments(response) {
   var str = '';
-  //console.log('building tournament')
   response.on('data', function (chunk) {
     str += chunk;
   });
   response.on('end', function(chunk) {
-    //console.log(str)
     parseTournaments(JSON.parse(str));
   });
 }
@@ -120,26 +116,34 @@ function getTournaments() {
   https.request(options, buildTournaments).end();
 }
 
+function sendFile(res, filename) {
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': 'text/html'})
+    res.end(content, 'utf-8')
+  })
+}
+
 //Build the database
-var file = "ranks.db"
 var exists = fs.existsSync(file)
 
 if (!exists) {
   console.log("Creating database")
   fs.openSync(file, "w")
 }
+
 var db = new sqlite3.Database(file);
 
 if (!exists) {
   db.serialize(function() {
     console.log("I'm in me mum's car")
       db.run("CREATE TABLE players (\
-      id INTEGER NOT NULL, \
-      tag TEXT(50) NOT NULL, \
-      win INTEGER(4), \
-      loss INTEGER(4),\
-      score INTEGER(4),\
-      PRIMARY KEY(id))")
+        id INTEGER NOT NULL, \
+        tag TEXT(50) NOT NULL, \
+        win INTEGER(4), \
+        loss INTEGER(4),\
+        score INTEGER(4),\
+        PRIMARY KEY(id))")
 
       db.run("CREATE TABLE matches(\
         id INT NOT NULL, \
@@ -150,23 +154,14 @@ if (!exists) {
         t_id int(4),\
         PRIMARY KEY(id))")
   })
+
+  //get all tournaments
   getTournaments();
-  
+
 }
 
-//get all tournaments
+//Schedule next server stuff
 var textSched = later.parse.text('at 12:00am every sunday');
 var timer = later.setInterval(getTournaments, textSched);
 server.listen(process.env.PORT || port);
 console.log('listening on 8080')
-
-// subroutines
-
-function sendFile(res, filename) {
-
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(content, 'utf-8')
-  })
-
-}
